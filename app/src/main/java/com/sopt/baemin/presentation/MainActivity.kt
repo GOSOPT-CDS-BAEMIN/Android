ca3345a61dc3291575ea040ba3ad2ecced1bd9f3
Kim hajeong <happykhj67@gmail.com>
2023-05-18 14:23:38 +0900

    [add] 메인액티비티 추가

---
+++ app/src/main/java/com/sopt/baemin/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.sopt.baemin.presentation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.sopt.baemin.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
