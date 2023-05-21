b9c03535a6c878f6a3cb2300de533bb21902c9dc
Kim hajeong <happykhj67@gmail.com>
2023-05-21 16:59:41 +0900

    [add] api client 추가

---
+++ app/src/main/java/com/sopt/baemin/data/api/ApiClient.kt
@@ -0,0 +1,33 @@
+package com.sopt.baemin.data.api
+
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+
+object ApiClient {
+    private const val BASE_URL = "http://18.235.5.238:8080/"
+    private var retrofit: Retrofit? = null
+
+    @OptIn(ExperimentalSerializationApi::class)
+    fun getRetrofit(): Retrofit {
+        if (retrofit == null) {
+            val logger = HttpLoggingInterceptor().apply {
+                level = HttpLoggingInterceptor.Level.BASIC
+                level = HttpLoggingInterceptor.Level.BODY
+            }
+            val client = OkHttpClient.Builder()
+                .addInterceptor(logger)
+                .build()
+            retrofit = Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .client(client)
+                .addConverterFactory(Json.asConverterFactory("application/json".toMediaType()))
+                .build()
+        }
+        return retrofit!!
+    }
+}
