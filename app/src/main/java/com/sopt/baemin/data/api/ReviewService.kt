d7113922b44a6a98238df62d32e601a77c51d1cb
Kim hajeong <happykhj67@gmail.com>
2023-05-23 10:21:43 +0900

    [add] ReviewService 추가

---
+++ app/src/main/java/com/sopt/baemin/data/api/ReviewService.kt
@@ -0,0 +1,13 @@
+package com.sopt.baemin.data.api
+
+import com.sopt.baemin.data.model.response.ReviewResponse
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface ReviewService {
+    @GET("review/{storeId}")
+    fun getReview(
+        @Path("storeId") storeId: Int
+    ): Call<ReviewResponse>
+}
