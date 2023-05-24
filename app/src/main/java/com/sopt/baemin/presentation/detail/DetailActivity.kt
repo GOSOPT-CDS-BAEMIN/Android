cc5e176ca1c307c4bb4c05d192610a4f2ff17d88
Kim hajeong <happykhj67@gmail.com>
2023-05-25 03:47:12 +0900

    [add] detail activity에 setVisibility()함수 추가

--- app/src/main/java/com/sopt/baemin/presentation/detail/DetailActivity.kt
+++ app/src/main/java/com/sopt/baemin/presentation/detail/DetailActivity.kt
@@ -1,7 +1,9 @@
 package com.sopt.baemin.presentation.detail
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.viewModels
+import androidx.core.widget.NestedScrollView
 import com.google.android.material.tabs.TabLayoutMediator
 import com.sopt.baemin.R
 import com.sopt.baemin.databinding.ActivityDetailBinding
@@ -9,10 +11,12 @@ import com.sopt.baemin.util.binding.BindingActivity
 
 class DetailActivity : BindingActivity<ActivityDetailBinding>(R.layout.activity_detail) {
     private val detailViewModel: DetailViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding.vm = detailViewModel
 
+        setVisibility()
         initAdapter()
     }
 
@@ -23,5 +27,15 @@ class DetailActivity : BindingActivity<ActivityDetailBinding>(R.layout.activity_
         TabLayoutMediator(binding.tlDetailOption, binding.vpDetailViewpager) { tab, position ->
             tab.text = tabTitleArray[position]
         }.attach()
+    }
+
+    private fun setVisibility() {
+        binding.ssvStoreDetail.setOnScrollChangeListener(NestedScrollView.OnScrollChangeListener { _, _, scrollY, _, _ ->
+            if (scrollY > 600) {
+                binding.clDetailHeader.visibility = View.VISIBLE
+            } else {
+                binding.clDetailHeader.visibility = View.GONE
+            }
+        })
     }
 }
