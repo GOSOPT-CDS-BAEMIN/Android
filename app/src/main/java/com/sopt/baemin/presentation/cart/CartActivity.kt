718ebcf3ddc0e0cc158f4aed74335352f0aa2365
leeeha <jxlhe46@seoultech.ac.kr>
2023-05-25 12:18:23 +0900

    [add] 장바구니 액티비티 추가

---
+++ app/src/main/java/com/sopt/baemin/presentation/cart/CartActivity.kt
@@ -0,0 +1,12 @@
+package com.sopt.baemin.presentation.cart
+
+import android.os.Bundle
+import com.sopt.baemin.R
+import com.sopt.baemin.databinding.ActivityCartBinding
+import com.sopt.baemin.util.binding.BindingActivity
+
+class CartActivity : BindingActivity<ActivityCartBinding>(R.layout.activity_cart) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
