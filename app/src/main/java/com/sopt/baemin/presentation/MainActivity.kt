e2c0b174770f4df598e0884c153d3541fee21333
jooyyoo <aliceyun9@naver.com>
2023-05-26 02:51:18 +0900

    [ui] 메뉴 상세 ui 구현 완료 및 MainActivity test

--- app/src/main/java/com/sopt/baemin/presentation/MainActivity.kt
+++ app/src/main/java/com/sopt/baemin/presentation/MainActivity.kt
@@ -3,9 +3,10 @@ package com.sopt.baemin.presentation
 import android.os.Bundle
 import com.sopt.baemin.R
 import com.sopt.baemin.databinding.ActivityMainBinding
+import com.sopt.baemin.databinding.ActivityMenuDetailBinding
 import com.sopt.baemin.util.binding.BindingActivity
 
-class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main) {
+class MainActivity : BindingActivity<ActivityMenuDetailBinding>(R.layout.activity_menu_detail) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
