643eaa24f2c04dcce3bcf0d511609a6e5f8a6e13
Kim hajeong <happykhj67@gmail.com>
2023-05-26 22:45:17 +0900

    [add] @SerialName 추가

--- app/src/main/java/com/sopt/baemin/data/model/response/MenuResponse.kt
+++ app/src/main/java/com/sopt/baemin/data/model/response/MenuResponse.kt
@@ -1,43 +1,68 @@
 package com.sopt.baemin.data.model.response
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class MenuResponse(
+    @SerialName("status")
     val status: Int,
+    @SerialName("success")
     val success: Boolean,
+    @SerialName("message")
     val message: String,
+    @SerialName("data")
     val data: StoreData
 )
 
 @Serializable
 data class StoreData(
+    @SerialName("storeInfo")
     val storeInfo: StoreInfo,
+    @SerialName("foods")
     val foods: List<Foods>
 )
 
 @Serializable
 data class StoreInfo(
+    @SerialName("storeId")
     val storeId: Int,
+    @SerialName("storeTypeId")
     val storeTypeId: Int,
+    @SerialName("storeType")
     val storeType: String,
+    @SerialName("storeName")
     val storeName: String,
+    @SerialName("deliveryTime")
     val deliveryTime: String,
+    @SerialName("description")
     val description: String,
+    @SerialName("minOrderPrice")
     val minOrderPrice: Int,
+    @SerialName("deliveryFee")
     val deliveryFee: Int,
+    @SerialName("rate")
     val rate: Double,
+    @SerialName("storeImages")
     val storeImages: List<String>,
+    @SerialName("couponExist")
     val couponExist: Boolean,
+    @SerialName("new")
     val new: Boolean
 )
 
 @Serializable
 data class Foods(
+    @SerialName("foodId")
     val foodId: Int,
+    @SerialName("foodName")
     val foodName: String,
+    @SerialName("price")
     val price: Int,
+    @SerialName("foodDescription")
     val foodDescription: String,
+    @SerialName("foodImageUrl")
     val foodImageUrl: String,
+    @SerialName("best")
     val best: Boolean
 )
--- app/src/main/java/com/sopt/baemin/data/model/response/ReviewResponse.kt
+++ app/src/main/java/com/sopt/baemin/data/model/response/ReviewResponse.kt
@@ -1,19 +1,28 @@
 package com.sopt.baemin.data.model.response
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class ReviewResponse(
+    @SerialName("status")
     val status: Int,
+    @SerialName("success")
     val success: Boolean,
+    @SerialName("message")
     val message: String,
+    @SerialName("data")
     val data: List<Review>
 )
 
 @Serializable
 data class Review(
+    @SerialName("nickname")
     val nickname: String,
+    @SerialName("reviewRate")
     val reviewRate: Double,
+    @SerialName("reviewImageUrl")
     val reviewImageUrl: String,
+    @SerialName("content")
     val content: String
 )
