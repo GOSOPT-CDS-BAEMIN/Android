04142e44a259464afcca6fb98d34bd86dab15d5c
leeeha <jxlhe46@seoultech.ac.kr>
2023-05-26 12:56:50 +0900

    [fix] NestedScrollView 사용해서 여러 뷰들 한번에 스크롤 가능하도록
    - ScrollView can host only one direct child 에러 발생
    - 스크롤뷰는 하나의 direct child만 가질 수 있기 때문에 여러 개의 뷰를 스크롤 하고 싶으면 그 뷰들을 레이아웃으로 감싸줘야 한다!!!

--- app/src/main/java/com/sopt/baemin/presentation/cart/StoreAdapter.kt
+++ app/src/main/java/com/sopt/baemin/presentation/cart/StoreAdapter.kt
@@ -4,23 +4,17 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.sopt.baemin.data.entity.StoreDummy
 import com.sopt.baemin.data.model.response.Store
 import com.sopt.baemin.databinding.ItemCartStoreBinding
 import com.sopt.baemin.util.ItemDiffCallback
 
-class StoreAdapter : ListAdapter<Store, StoreAdapter.StoreViewHolder>(diffUtil) {
+class StoreAdapter : ListAdapter<StoreDummy, StoreAdapter.StoreViewHolder>(diffUtil) {
     class StoreViewHolder(private val binding: ItemCartStoreBinding) :
         RecyclerView.ViewHolder(binding.root) {
-        fun bind(store: Store) {
-            binding.store = store
-            initFoodAdapter(store)
-        }
+        fun bind(storeDummy: StoreDummy) {
+            binding.storeDummy = storeDummy
 
-        // 한 매장을 바인딩 시킬 때마다 food adapter 재생성 (일단 해보자)
-        private fun initFoodAdapter(store: Store) {
-            val foodAdapter = FoodAdapter()
-            binding.rvStoreFood.adapter = foodAdapter
-            foodAdapter.submitList(store.foods)
         }
     }
 
@@ -39,7 +33,7 @@ class StoreAdapter : ListAdapter<Store, StoreAdapter.StoreViewHolder>(diffUtil) 
     }
 
     companion object {
-        private val diffUtil = ItemDiffCallback<Store>(
+        private val diffUtil = ItemDiffCallback<StoreDummy>(
             onItemsTheSame = { old, new -> old === new },
             onContentsTheSame = { old, new -> old == new }
         )
