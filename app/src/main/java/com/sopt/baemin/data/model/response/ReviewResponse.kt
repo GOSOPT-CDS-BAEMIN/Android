1e0736c0f3d1ee3f248e70438909bd52e29d4924
Kim hajeong <happykhj67@gmail.com>
2023-05-27 01:51:17 +0900

    [fix] 리스폰스 수정

--- app/src/main/java/com/sopt/baemin/data/model/response/ReviewResponse.kt
+++ app/src/main/java/com/sopt/baemin/data/model/response/ReviewResponse.kt
@@ -1,28 +1,20 @@
 package com.sopt.baemin.data.model.response
 
-import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class ReviewResponse(
-    @SerialName("status")
     val status: Int,
-    @SerialName("success")
     val success: Boolean,
-    @SerialName("message")
     val message: String,
-    @SerialName("data")
     val data: List<Review>
 )
 
 @Serializable
 data class Review(
-    @SerialName("nickname")
+    val reviewId: Int,
     val nickname: String,
-    @SerialName("reviewRate")
     val reviewRate: Double,
-    @SerialName("reviewImageUrl")
     val reviewImageUrl: String,
-    @SerialName("content")
     val content: String
 )
